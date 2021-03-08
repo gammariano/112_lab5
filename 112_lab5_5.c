f8704f1d05bd53d763eec02ce7f2469d739344a8
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-08 19:38:54 +0700

    Max Sum

--- 112_lab5_5.c
+++ 112_lab5_5.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,k,m,n;
+    int i,j,k=0,l,m,n;
     int num[10][10];
     scanf("%d" ,&n);
     scanf("%d" ,&m);
@@ -12,6 +12,14 @@ int main()
             scanf("%d" ,&num[i][j]);
         }
     }
-
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            num[i][j]+=k;
+            k=num[i][j];
+        }
+    }
+    printf("%d " ,k);
     return 0;
 }
