c90e4856604d2f173a401055141374e84a3d2bf3
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-08 17:05:30 +0700

    Star

--- 112_lab5_2.c
+++ 112_lab5_2.c
@@ -4,13 +4,13 @@ int main()
     int i,j,k,n,m;
     scanf("%d" ,&n);
     m=n;
-    for(i=1;i<=n;i++)
+    for(i=1;i<n;i++)
     {
-        for(j=1;j<=m-1;j++)
+        for(j=1;j<m-1;j++)
         {
             printf("_");
         }
-        for(k=0;k<=n;k++)
+        for(k=0;k<=2*i-1;k++)
         {
             printf("*");
         }
