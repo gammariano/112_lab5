5008ad1fab816d216e73baa878bb5004a122e155
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-08 17:37:21 +0700

    Stack

--- 112_lab5_1.c
+++ 112_lab5_1.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
     int n,i,j,k,x=0,y;
@@ -32,12 +33,17 @@ int main()
                 y=j-k-1;
                 switch(y)
                 {
-                    case 0: b[x]+=(num[k]-48); break;
+                    case 0 : b[x]+=(num[k]-48); break;
+                    case 1 : b[x]+=(num[k]-48)*10; break;
+                    case 2 : b[x]+=(num[k]-48)*100; break;
+                    case 3 : b[x]+=(num[k]-48)*1000; break;
+                    case 4 : b[x]+=(num[k]-48)*10000; break;
+                    case 5 : b[x]+=(num[k]-48)*100000; break;
                 }
             }
+            x++;
         }
-
     }
-
+    printf("%d\n" ,b[0]);
     return 0;
 }
