315fb8b2fe38e32642d18c7792e68a8f7549465b
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-08 17:09:40 +0700

    Star

--- 112_lab5_2.c
+++ 112_lab5_2.c
@@ -1,18 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,k,n,m;
+    int i,j,k,l,n,m;
     scanf("%d" ,&n);
     m=n;
-    for(i=1;i<n;i++)
+    for(i=1;i<=n;i++)
     {
-        for(j=1;j<m-1;j++)
+        for(j=1;j<=m-1;j++)
         {
             printf("_");
         }
-        for(k=0;k<=2*i-1;k++)
+        for(k=0;k<=2*i-2;k++)
         {
             printf("*");
+        }
+        for(l=1;l<m;l++)
+        {
+            printf("_");
         }
         m--;
         printf("\n");
