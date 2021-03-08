5bab823b97dee5e28dc3243f0fb568eff1258a27
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-08 19:01:06 +0700

    Ugly Number

--- 112_lab5_3.c
+++ 112_lab5_3.c
@@ -5,9 +5,15 @@ int UglyNumber(int n)
 }
 int main()
 {
-    int n;
+    int n,i,j,k,m,c;
     scanf("%d" ,&n);
-    UglyNumber(n);
-    printf("%d" ,n);
+    int num[10000]={0};
+    num[0]=1;
+    m=1;
+    for(i=2;i<n*n*2;i++)
+    {
+        c=UglyNumber(i);
+
+    }
     return 0;
 }
